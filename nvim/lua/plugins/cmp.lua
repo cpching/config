fa7ac7286e84062dad82ba8e2ee4c28e989cb4f7
Pin-Ching Chen <p.ching0811@gmail.com>
2023-11-26 18:33:18 +0800

    Add: snippets.lua

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -1,40 +1,37 @@
-local check_backspace = function()                                                                                                  local col = vim.fn.col "." - 1
+local check_backspace = function()
+    local col = vim.fn.col "." - 1
     return col == 0 or vim.fn.getline("."):sub(col, col):match "%s"
 end
 --   פּ ﯟ   some other good icons
 local kind_icons = {
-  Text = "󰊄",
-  Method = "m",
-  Function = "󰊕",
-  Constructor = "",
-  Field = "",
-  Variable = "󰫧",
-  Class = "",
-  Interface = "",
-  Module = "",
-  Property = "",
-  Unit = "",
-  Value = "",
-  Enum = "",
-  Keyword = "󰌆",                                                                                                                    Snippet = "",                                                                                                                    Color = "",
-  File = "",
-  Reference = "",
-  Folder = "",
-  EnumMember = "",
-  Constant = "",
-  Struct = "",
-  Event = "",
-  Operator = "",
-  TypeParameter = "󰉺",
+    Text = "󰊄",
+    Method = "m",
+    Function = "󰊕",
+    Constructor = "",
+    Field = "",
+    Variable = "󰫧",
+    Class = "",
+    Interface = "",
+    Module = "",
+    Property = "",
+    Unit = "",
+    Value = "",
+    Enum = "",
+    Keyword = "󰌆",
+    Snippet = "",
+    Color = "",
+    File = "",
+    Reference = "",
+    Folder = "",
+    EnumMember = "",
+    Constant = "",
+    Struct = "",
+    Event = "",
+    Operator = "",
+    TypeParameter = "󰉺",
 }
 return 
 {
-    {
-        "rafamadriz/friendly-snippets",
-        config = function()
-            require("luasnip.loaders.from_vscode").lazy_load()
-        end,
-    },
     {
         "hrsh7th/nvim-cmp",
         event = "InsertEnter",
