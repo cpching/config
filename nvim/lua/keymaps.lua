b7082da9f649d5962422507d062bef754ef08107
Pin-Ching Chen <p.ching0811@gmail.com>
2023-11-27 19:42:25 +0800

    Edit: key mapping

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -35,7 +35,7 @@ keymap("v", "<M-j>", ":m .+1<CR>==", opts)
 keymap("v", "<M-k>", ":m .-2<CR>==", opts)
 -- Move text up and down
 
-keymap("v", "p", '"_dP', opts)
+keymap("v", "p", "\"_dP", opts)
 ---- Visual ----
 
 -- Visual Block --
